8d9a8fe03504cc7acccbdad1891cd1892678f427
NsirOne <nsirone@outlook.com>
2016-04-27 10:43:53 +0800

    win

---
+++ python/20160426/dierge.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# @version: 1.0
+# @author: Nsirone
+# @license: Apache Licence 
+# @contact: nsirone@outlook.com
+# @site: http://www.google.com
+# @software: PyCharm Community Edition
+# @file: dierge.py
+# @time: 2016/4/27 10:41
+
--- python/python_study2/History.py
+++ python/python_study2/History.py
@@ -23,7 +23,10 @@ def get_history():
 	f= opener.open(url2)
 	html = f.read()
 	response = json.loads(html)
-	return response
+	# print type(html)
+	return html
+	# print type(response)
+	# return response
 # 增加播放历史信息
 def add_history(i):
 	url = 'http://api.bilibili.com/x/v2/history/add'
